547dfb8c38a6558c704e8bfe78d7408eaeafa5c9
Hessa <hessa.m.muhanna@gmail.com>
2019-01-22 19:46:10 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Hessa Mohamed on 22/01/2019.
+//  Copyright © 2019 Hessa Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
